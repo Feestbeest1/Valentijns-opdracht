0aea0c7cad760a026378c319c2f3add4f1c77a46
Feestbeest1 <d.mateman18@gmail.com>
2025-02-04 10:21:49 +0100

    push

--- script.js
+++ script.js
@@ -42,6 +42,19 @@ function deelLink() {
     }
 }
 
+document.getElementById('fotoUpload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const preview = document.getElementById('preview');
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
 
 
 setInterval(maakHart, 500);
